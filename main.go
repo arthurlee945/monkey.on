b38c9650ef1a04a234f7f3d45f17e9431ba74a47
Arthur Lee <arthur.lee945@gmail.com>
2024-01-13 17:49:17 -0600

    playing with pointer

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
-	nana "fmt"
+	"fmt"
 )
 
+func testingFunc(val *int) *int {
+	fmt.Printf("%v : is where original int passed was pointing at value of %v\n", val, *val)
+	randint := 5095328
+	val = &randint
+	return val
+}
+
 func main() {
-	nana.Println("Hello World?")
+	fmt.Println("Hello World?")
+	testint := 1994
+	var newVal = testingFunc(&testint)
+	fmt.Printf("%v : is where original int passed was pointing at value of %v\n", newVal, *newVal)
 }
