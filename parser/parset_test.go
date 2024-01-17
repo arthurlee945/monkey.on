adf618d5217a53a8a7390731a91f4e992ea8bf4e
Arthur Lee <arthur.lee945@gmail.com>
2024-01-16 21:18:33 -0600

    adding parser pkg and test

---
+++ parser/parset_test.go
@@ -0,0 +1,65 @@
+package parser
+
+import (
+	"testing"
+
+	"github.com/arthurlee945/monkey.on/ast"
+	"github.com/arthurlee945/monkey.on/lexer"
+)
+
+func TestLetStatements(t *testing.T) {
+	input := `
+	let x = 5;
+	let y = 10;
+	let foobar = 8348583;
+	`
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	if program == nil {
+		t.Fatalf("ParseProgram() returned nil")
+	}
+
+	if len(program.Statements) <= 3 {
+		t.Fatalf("program.Statments does not contain 3 statements. got=%d", len(program.Statements))
+	}
+
+	test := []struct {
+		expectedIdentifier string
+	}{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
+
+	for i, tt := range test {
+		stmt := program.Statements[i]
+		if !testLetStatements(t, stmt, tt.expectedIdentifier) {
+			return
+		}
+	}
+}
+
+func testLetStatements(t *testing.T, s ast.Statement, name string) bool {
+	if s.TokenLiteral() != "let" {
+		t.Errorf("s.TokenLiteral not 'let'. got=%q", s.TokenLiteral())
+		return false
+	}
+
+	letStmt, ok := s.(*ast.LetStatement)
+	if !ok {
+		t.Errorf("s can not be casted to type *ast.LetStatement. got=%q", s)
+		return false
+	}
+	if letStmt.Name.Value != name {
+		t.Errorf("letStmt.Name.Value not '%s'. got=%s", name, letStmt.Name.Value)
+		return false
+	}
+	if letStmt.Name.TokenLiteral() != name {
+		t.Errorf("s.Name not '%s'. got=%s", name, letStmt.Name)
+		return false
+	}
+
+	return true
+}
