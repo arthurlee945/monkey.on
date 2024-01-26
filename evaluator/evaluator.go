acd3879e7482ce17568057277a926d7dc3f48d5c
Arthur Lee <arthur.lee945@gmail.com>
2024-01-26 15:09:58 -0600

    add support for indexing array

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -63,6 +63,16 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 			return elements[0]
 		}
 		return &object.Array{Elements: elements}
+	case *ast.IndexExpression:
+		left := Eval(node.Left, env)
+		if isError(left) {
+			return left
+		}
+		index := Eval(node.Index, env)
+		if isError(index) {
+			return index
+		}
+		return evalIndexExpression(left, index)
 	case *ast.Boolean:
 		return nativeBoolToBooleanObject(node.Value)
 	case *ast.PrefixExpression:
@@ -198,6 +208,28 @@ func evalPrefixExpression(operator string, right object.Object) object.Object {
 		return newError("unknown operator: %s%s", operator, right.Type())
 
 	}
+}
+
+func evalIndexExpression(left, index object.Object) object.Object {
+	switch {
+	case left.Type() == object.ARRAY_OBJ && index.Type() == object.INTEGER_OBJ:
+		return evalArrayIndexExpression(left, index)
+	default:
+		return newError("index operator not supported: %s", left.Type())
+	}
+}
+
+func evalArrayIndexExpression(array, index object.Object) object.Object {
+	arrayObj := array.(*object.Array)
+	idx := index.(*object.Integer).Value
+	max := int64(len(arrayObj.Elements) - 1)
+
+	if idx > max || idx < 0 {
+		//newError("index out of bound.")
+		return NULL
+	}
+
+	return arrayObj.Elements[idx]
 }
 
 func evalInfixExpression(operator string, left, right object.Object) object.Object {
